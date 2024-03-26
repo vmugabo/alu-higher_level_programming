ad0b7b04e2d03e6e739abecb32bd307fdd6158d7
Vincent Mugabo <63720396+vmugabo@users.noreply.github.com>
2024-03-26 16:30:50 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-[ $# -ne 1 ] && { echo "Usage: $0 <URL>"; exit 1; }
-curl -s "$1" | wc -c
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
