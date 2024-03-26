f71a30b1c7bdb57256cce9bfa51bd5a960bf6e54
Vincent Mugabo <63720396+vmugabo@users.noreply.github.com>
2024-03-26 16:27:41 +0200

    attempt solve

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
+[ $# -ne 1 ] && { echo "Usage: $0 <URL>"; exit 1; }
 curl -s "$1" | wc -c
