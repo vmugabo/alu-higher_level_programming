d9ed1b1de717cb77b015100b84349e4dd76992af
Vincent Mugabo <63720396+vmugabo@users.noreply.github.com>
2024-04-18 15:06:56 +0200

    correct indent

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 """Define class with private __nb_objects """
+
+
 class Base:
+"""Base Class with __nb_objects as private class attribute"""
 
-
-    """Base Class with __nb_objects as private class attribute"""
-    
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -15,4 +15,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
